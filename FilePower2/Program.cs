58620973d8db4bde4aca3cbb79aa61c6c30b6297
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-08 23:44:30 +0100

    FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input_path = args[0];
+            string input_string;
+            bool input_loop = true;
+            using StreamWriter sw = new StreamWriter(input_path);
+
+            while(input_loop == true)
+            {
+                input_string = Console.ReadLine();
+                if(input_string == "")
+                {
+                    input_loop = false;
+                }
+                else
+                {
+                    sw.WriteLine(input_string);
+                }
+            }
+        }
+    }
+}
