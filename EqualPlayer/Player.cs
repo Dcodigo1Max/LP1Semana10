764f0ef5f935d279d90b0991c1bc63058aa46843
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-09 10:17:58 +0100

    Changed Program name

---
+++ EqualPlayer/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EqualPlayer
+{
+    public class Player
+    {
+        public PlayerClass PClass {get;}
+        public string Name {get;}
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass = pClass;
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return PClass.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public override bool Equals(object other)
+        {
+            bool equal_value;
+            Player otherPlayer = other as Player;
+            if(otherPlayer is null) 
+                equal_value = false;
+            else if(PClass == otherPlayer.PClass && Name == otherPlayer.Name) 
+                equal_value = true;
+            else
+                return false;
+            return equal_value;
+        }
+
+
+
+
+    }
+}
