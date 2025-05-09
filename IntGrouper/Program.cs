cbaa39357b3e5b402b83f0abc796c006aa1efa3c
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-09 17:43:11 +0100

    Grouper code p1

---
+++ IntGrouper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace IntGrouper
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input_numbers = args[0];
+            string input_lines;
+            bool input_real = true;
+            int numbers = Convert.ToInt32(input_lines);
+            int soma = numbers + numbers;
+
+            while( input_real = true)
+            {
+                input_lines = Console.ReadLine();
+
+                if( numbers in input_lines != )
+                {
+
+                }
+
+            }
+
+
+            foreach(n in numbers)
+            {
+
+            }
+
+            File.WriteAllLines(input_numbers, );
+
+
+
+        }
+    }
+}
