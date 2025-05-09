795dac0db2f372a6a58b51a15f7e6d23263354d3
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-09 10:21:14 +0100

    Added part 2 of Exer 4

---
+++ EqualPlayer/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace EqualPlayer
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            HashSet<Player> setofPlayers = new HashSet<Player>(){new Player
+            (PlayerClass.Tank, "Ana"), 
+            new Player(PlayerClass.Slayer,"Paulo"),
+            new Player(PlayerClass.Tank,"Ana")};
+
+            foreach(Player p in setofPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
+        }
+    }
+}
